d61a2e6c0ce2adb5e5b6b226f3fa35ce026692fc
DTS <officialdts11@gmail.com>
2025-01-28 20:16:32 +0530

    sort import

--- auth.ts
+++ auth.ts
@@ -1,8 +1,8 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import db from "./lib/db/db";
 import { handleGitHubSignIn } from "./lib/auth/github-handler";
+import db from "./lib/db/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
