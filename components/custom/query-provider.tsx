28e6d1496b07c1fee68c71628313e5d3fce90e10
Abin Thomas <followgamer102@gmail.com>
2025-02-01 12:42:15 +0530

    feat: additionally added sessionProvider to wrap

--- components/custom/query-provider.tsx
+++ components/custom/query-provider.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
-
-export function QueryProvider({ children }: { children: React.ReactNode }) {
+import {Session} from "next-auth";
+import {SessionProvider}  from "next-auth/react";
+export function QueryProvider({ children, session }: { children: React.ReactNode, session: Session | null }) {
   return (
+    <SessionProvider session={session}>
     <QueryClientProvider client={new QueryClient()}>
       {children}
     </QueryClientProvider>
+    </SessionProvider>
   );
 }
