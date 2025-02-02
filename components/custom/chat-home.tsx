562f3eb073c292ecf6bc2be8d863fca2272f3953
Abin Thomas <followgamer102@gmail.com>
2025-02-02 22:12:20 +0530

    fix: align of text

--- components/custom/chat-home.tsx
+++ components/custom/chat-home.tsx
@@ -12,7 +12,7 @@ export function ChatHome() {
   return (
     <div className="flex flex-col justify-center gap-4 h-full p-4 items-center overflow-y-auto scroller">
       <div className="flex flex-col justify-center items-center gap-9">
-        <h6 className="text-4xl font-semibold">What can I help with?</h6>
+        <h6 className="text-4xl text-center font-semibold">What can I help with?</h6>
           <Button className="rounded-lg shadow-[0_5px_15px_rgba(192,192,192,0.4)] hover:shadow-[0_8px_25px_rgba(192,192,192,0.6)] transition-shadow duration-300" onClick={handleIdRe}>
             Get Started
           </Button>
