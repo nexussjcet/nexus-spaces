52194b8d7c8e32c58cc85cb6c91e71943c4abd83
Abin Thomas <followgamer102@gmail.com>
2025-02-02 21:59:45 +0530

    minor ui chnages in chat-home

--- components/custom/chat-home.tsx
+++ components/custom/chat-home.tsx
@@ -1,7 +1,22 @@
+"use client"
+import { Button } from "../ui/button";
+import { redirect } from "next/navigation";
+import { useSession } from "next-auth/react";
 export function ChatHome() {
+  const { data: session } = useSession();
+  const id = session?.user?.id;
+  const handleIdRe = () => {
+    redirect(`/${id}`)
+  }
+  
   return (
-    <div className="flex flex-col-reverse gap-4 h-full p-4 items-center overflow-y-auto scroller">
-      Home
+    <div className="flex flex-col justify-center gap-4 h-full p-4 items-center overflow-y-auto scroller">
+      <div className="flex flex-col justify-center items-center gap-9">
+        <h6 className="text-4xl font-semibold">What can I help with?</h6>
+          <Button className="rounded-lg shadow-[0_5px_15px_rgba(192,192,192,0.4)] hover:shadow-[0_8px_25px_rgba(192,192,192,0.6)] transition-shadow duration-300" onClick={handleIdRe}>
+            Get Started
+          </Button>
+      </div>
     </div>
     // Create Home Page
   );
