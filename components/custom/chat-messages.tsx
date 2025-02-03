bf5346882579fbe98842b063b4fa3f7ee207d339
Aravind Manoj <aravindmng47@gmail.com>
2025-02-03 11:34:59 +0530

    Added skeleton effect to sidebar and chat section

--- components/custom/chat-messages.tsx
+++ components/custom/chat-messages.tsx
@@ -1,14 +1,16 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { fetchConversation } from "@/lib/handler";
 import { useChatContext } from "@/contexts/chat";
 import { useParams, useRouter } from "next/navigation";
 import MarkdownRender from "./markdown-render";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function ChatPage() {
   const convId = useParams().id;
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   const {
     setSelectedConversation,
@@ -21,7 +23,10 @@ export function ChatPage() {
       const res = await (await fetchConversation(id)).json();
       if (res.success) {
         setConversation(res.data);
-      }else {
+        setTimeout(() => {
+          setLoading(false);
+        }, 500);
+      } else {
         router.push("/chat");
       }
     };
@@ -32,35 +37,61 @@ export function ChatPage() {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div className="flex flex-col-reverse gap-4 h-full p-4 items-center overflow-y-auto scroller">
-      {conversation?.messages?.toReversed().map((chatMessage) => (
-        <div
-          className={`flex flex-col w-full max-w-[700px] ${chatMessage.isUser ? 'items-end' : ''}`}
-          key={chatMessage.id}
-        >
-          <div className={`flex items-start gap-2 ${chatMessage.isUser ? 'flex-row-reverse' : ''}`}>
-            {!chatMessage.isUser && (
-              <div className="w-6 h-6 rounded-full overflow-hidden flex-shrink-0 mt-1">
-                <Image
-                  src="/nexus.webp"
-                  width={24}
-                  height={24}
-                  alt="Spacey"
-                  className="w-full h-full object-cover"
-                />
-              </div>
-            )}
-            <div className={`flex flex-col ${chatMessage.isUser ? 'items-end' : ''}`}>
-              <h3 className="font-bold text-neutral-400">
-                {chatMessage.isUser ? "You" : "Spacey"}
-              </h3>
-              <div className={`max-w-full ${chatMessage.isUser ? 'bg-blue-600 rounded-xl px-4 py-2' : ''}`}>
-                <MarkdownRender>{chatMessage.content.text}</MarkdownRender>
-              </div>
+    <>
+      {loading ? (
+        <div className="flex flex-col-reverse h-full p-[5%] items-center overflow-y-auto scroller gap-4 md:gap-0 mb-4 md:mb-0">      {/* Avatar Skeleton */}
+          <div className="flex flex-row w-full max-w-[700px] gap-2 md:gap-4">
+            <Skeleton className="bg-gray-600 w-[50px] h-[50px] rounded-full" />
+            <div className="w-[80%] space-y-3">
+              <Skeleton className="bg-gray-600 h-[2vh] w-[90%]" />
+              <Skeleton className="bg-gray-600 h-[2vh] w-[90%]" />
+              <Skeleton className="bg-gray-600 h-[2vh] w-[90%]" />
+              <Skeleton className="bg-gray-600 h-[2vh] w-[80%]" />
+              <Skeleton className="bg-gray-600 h-[2vh] w-[85%]" />
+              <Skeleton className="bg-gray-600 h-[2vh] w-[70%]" />
             </div>
           </div>
+          <div className="flex flex-row w-full max-w-[700px] gap-2 md:gap-4 mb-[5%]">
+            <div className="w-[80%] space-y-3">
+            <Skeleton className="bg-gray-600 h-[2vh] w-[90%] ml-[10%]" />
+              <Skeleton className="bg-gray-600 h-[2vh] w-[90%] ml-[10%]" />
+              <Skeleton className="bg-gray-600 h-[2vh] w-[80%] ml-[20%]" />
+            </div>
+            <Skeleton className="bg-gray-600 w-[50px] h-[50px] rounded-full" />
+          </div>
         </div>
-      ))}
-    </div>
+      ) : (
+        <div className="flex flex-col-reverse gap-4 h-full p-4 items-center overflow-y-auto scroller">
+          {conversation?.messages?.toReversed().map((chatMessage) => (
+            <div
+              className={`flex flex-col w-full max-w-[700px] ${chatMessage.isUser ? 'items-end' : ''}`}
+              key={chatMessage.id}
+            >
+              <div className={`flex items-start gap-2 ${chatMessage.isUser ? 'flex-row-reverse' : ''}`}>
+                {!chatMessage.isUser && (
+                  <div className="w-6 h-6 rounded-full overflow-hidden flex-shrink-0 mt-1">
+                    <Image
+                      src="/nexus.webp"
+                      width={24}
+                      height={24}
+                      alt="Spacey"
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                )}
+                <div className={`flex flex-col ${chatMessage.isUser ? 'items-end' : ''}`}>
+                  <h3 className="font-bold text-neutral-400">
+                    {chatMessage.isUser ? "You" : "Spacey"}
+                  </h3>
+                  <div className={`max-w-full ${chatMessage.isUser ? 'bg-blue-600 rounded-xl px-4 py-2' : ''}`}>
+                    <MarkdownRender>{chatMessage.content.text}</MarkdownRender>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div >
+      )}
+    </>
   );
 }
