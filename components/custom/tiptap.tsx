94b942590b9103e89b1caad41f15906d5681910c
DTS <officialdts11@gmail.com>
2025-02-04 22:30:22 +0530

    actual rich text editor suing tiptap

---
+++ components/custom/tiptap.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Heading from "@tiptap/extension-heading";
+import Image from "@tiptap/extension-image";
+import Link from "@tiptap/extension-link";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Toolbar from "./toolbar";
+
+export default function Tiptap({
+  description,
+  onChange,
+}: {
+  description: string;
+  onChange: (richText: string) => void;
+}) {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Heading.configure({
+        levels: [1, 2],
+      }),
+      Link.configure({
+        openOnClick: true,
+        autolink: true,
+        linkOnPaste: true,
+      }),
+      Image,
+    ],
+    content: description,
+    editorProps: {
+      attributes: {
+        class:
+          "rounded-b-md border min-h-[150px] border-input bg-transparent p-2 text-white",
+      },
+    },
+    onUpdate: ({ editor }) => {
+      onChange(editor.getHTML());
+    },
+  });
+
+  return (
+    <div className="flex flex-col gap-0 border border-gray-300 rounded-md">
+      <Toolbar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  );
+}
