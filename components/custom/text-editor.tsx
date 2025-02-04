71b6f9920405098a7b9ae84fbdeef77c27a0355e
DTS <officialdts11@gmail.com>
2025-02-04 22:30:08 +0530

    text-editor component

---
+++ components/custom/text-editor.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import Tiptap from "./tiptap";
+
+export default function TextEditor() {
+  const formSchema = z.object({
+    text: z.string().min(1, { message: "Text is required" }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    mode: "onChange",
+    defaultValues: {
+      text: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data);
+    // submit logic goes here... (like POST to db etc)
+  }
+
+  return (
+    <main className="p-24">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="text"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Text</FormLabel>
+                <FormControl>
+                  <Tiptap
+                    description="Enter your text here"
+                    onChange={field.onChange}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Submit
+          </button>
+        </form>
+      </Form>
+    </main>
+  );
+}
