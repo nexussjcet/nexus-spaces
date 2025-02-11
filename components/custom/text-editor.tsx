a551fc001985039d17f81e1acfe52fed8b134f99
DTS <officialdts11@gmail.com>
2025-02-11 23:35:32 +0530

    manage post + add title form field

--- components/custom/text-editor.tsx
+++ components/custom/text-editor.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { redirect, useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import {
@@ -14,21 +18,39 @@ import {
 import Tiptap from "./tiptap-dynamic";
 
 export default function TextEditor() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    redirect("/signin");
+  }
+
+  const router = useRouter();
+
   const formSchema = z.object({
-    text: z.string().min(1, { message: "Text is required" }),
+    title: z.string().min(1, { message: "Title is required" }),
+    content: z.string().min(1, { message: "Content is required" }),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     mode: "onChange",
     defaultValues: {
-      text: "",
+      title: "",
+      content: "",
+    },
+  });
+
+  const mutation = useMutation({
+    mutationFn: (newPost: z.infer<typeof formSchema>) => {
+      return axios.post("/api/posts", newPost);
+    },
+    onSuccess: () => {
+      router.push("/posts");
     },
   });
 
   function onSubmit(data: z.infer<typeof formSchema>) {
-    console.log(data);
-    // submit logic goes here... (like POST to db etc)
+    mutation.mutate(data);
   }
 
   return (
@@ -37,13 +59,30 @@ export default function TextEditor() {
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
             control={form.control}
-            name="text"
+            name="title"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Text</FormLabel>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <input
+                    {...field}
+                    className="bg-transparent text-white w-full p-2 border border-gray-300 rounded-md"
+                    placeholder="Enter post title"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Content</FormLabel>
                 <FormControl>
                   <Tiptap
-                    description="Enter your text here"
+                    description="Enter your post content here"
                     onChange={field.onChange}
                   />
                 </FormControl>
