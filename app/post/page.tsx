20e3cc8541fb204079e4b3e2e2c6458c9760f41f
DTS <officialdts11@gmail.com>
2025-02-12 14:12:36 +0530

    fix weird background styling

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -2,7 +2,7 @@ import TextEditor from "@/components/custom/text-editor";
 
 export default function PostPage() {
   return (
-    <div className="w-full h-screen bg-black text-white">
+    <div className="fixed inset-0 bg-black text-white overflow-y-auto">
       <TextEditor />
     </div>
   );
