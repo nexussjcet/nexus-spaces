c5eb051fdc1c6a5cd47d0cf90558df436af4e3e1
DTS <officialdts11@gmail.com>
2025-02-15 00:27:20 +0530

    use navbar component

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/custom/navbar";
 import TextEditor from "@/components/custom/text-editor";
 
 export default function PostPage() {
   return (
     <div className="fixed inset-0 bg-black text-white overflow-y-auto">
+      <Navbar />
       <TextEditor />
     </div>
   );
