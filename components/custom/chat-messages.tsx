15c3d98fc6a8bf9d4f1421d5ccd7ee61726d18b4
Aravind Manoj <aravindmng47@gmail.com>
2025-02-15 13:08:44 +0530

    [FIX] User text overflow issue + proper wrapping

--- components/custom/chat-messages.tsx
+++ components/custom/chat-messages.tsx
@@ -101,7 +101,7 @@ export function ChatMessages() {
                     {!chatMessage.isUser && "Spacey"}
                   </h3>
                   {chatMessage.isUser ? (
-                    <div className="bg-white text-black rounded-xl px-4 py-2 mt-6">
+                    <div className="bg-white text-black rounded-xl px-4 py-2 mt-6 whitespace-pre-wrap break-all">
                       {chatMessage.content.text}
                     </div>
                   ) : (
